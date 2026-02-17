3e83c3cf08b21509fd4809a0174fc2b57e54394b
himani22122002-hue <himani22122002@gmail.com>
2026-02-17 23:14:38 +0530

    Added basic task manager project using Tkinter

--- Hello.py
+++ Hello.py
@@ -1,1 +1,24 @@
 print("Hello")
+def add(a, b):
+    print("sum=",a+b)
+add(2,3)
+
+fact=1
+num=5
+for i in range(1,num+1):
+    fact=fact*i
+    print("factorial=",fact)
+    
+#num = int(input("Enter number: "))
+#fact = 1
+#for i in range(1, num + 1):
+    #fact = fact * i
+#print("Factorial =", fact)
+
+def factorial(n):
+    fact=1
+    for i in range(1,n+1):
+        fact=fact*i
+    return fact
+num = int(input("Enter number: "))
+print("Factorial =", factorial(num))
